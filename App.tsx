9af947f9f658715270d7584542d5c7253b564e3b
10xvick <103027108+10xvick@users.noreply.github.com>
2023-07-18 13:37:44 +0530

    1 file modified

--- App.tsx
+++ App.tsx
@@ -2,15 +2,15 @@ import * as React from 'react';
 import './style.css';
 import CodeWindow from './Code/CodeWindow';
 import GlobalContext, { GlobalReducer, Store } from './StateManager/Context';
-import { getword, Text2Speech } from './Speech/Speech';
+import { Text2Speech } from './Speech/Speech';
 
 export default function App() {
   const [state, dispatch] = React.useReducer(GlobalReducer, Store);
-
+  const [start, setStart] = React.useState(false);
   return (
     <div>
       <GlobalContext.Provider value={{ state: state, dispatch: dispatch }}>
-        <Main />
+        <div onClick={() => setStart((e) => !e)}>{start && <Main />}</div>
       </GlobalContext.Provider>
     </div>
   );
@@ -22,11 +22,13 @@ const Main = () => {
     dispatch,
   } = React.useContext<any>(GlobalContext);
 
-  const [text,setText] = React.useState('');
+  const [text, setText] = React.useState('');
 
   React.useEffect(() => {
-    const next = () => setTimeout(() => {dispatch({ type: 'nextStep' });
-    }, 500);  
+    const next = () =>
+      setTimeout(() => {
+        dispatch({ type: 'nextStep' });
+      }, 500);
     Text2Speech(speech, next, setText);
   }, [step]);
 
